57ca80cbdf792ee56a2020f3ba8e330f59c4b8aa
Nikita Koval <69389602+Mysterion47@users.noreply.github.com>
2021-03-26 02:18:02 +0300

    Let's begin !

---
+++ js/watch.js
@@ -0,0 +1,11 @@
+function startTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    document.getElementById("time").innerHTML = hours + ":"+ minutes + ":" + seconds;
+    setTimeout(startTime, 1000);
+  }
